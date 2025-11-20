2f0b169526c52673b822073f3e8f07ffc83ac665
geobas <ksenera@yahoo.com>
2025-11-20 12:21:51 +0200

    Setup mongodb

--- config/database.php
+++ config/database.php
@@ -31,6 +31,12 @@ return [
 
     'connections' => [
 
+        'mongodb' => [
+            'driver'   => 'mongodb',
+            'dsn'      => env('MONGODB_URI'),
+            'database' => env('MONGODB_DATABASE'),
+        ],
+
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
